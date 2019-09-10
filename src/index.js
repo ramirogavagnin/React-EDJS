c2c646a577e70f92305bb0a320b226c78a1d3b11
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2019-09-10 13:05:43 -0300

    Estilos Header y base App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
-const App = document.getElementById('app')
-ReactDOM.render(<HelloWorld />, App)
+import App from './containers/App'
+
+ReactDOM.render(<App />, document.getElementById('app'))
