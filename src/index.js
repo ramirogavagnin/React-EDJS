151191619c97088219c1e3dc911e1771bc78688e
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2019-09-09 23:26:34 -0300

    Webpack config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+const App = document.getElementById('app')
+ReactDOM.render(<HelloWorld />, App)
