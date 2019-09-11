8f9571b74645f430742a2132b11ce10af9460df3
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2019-09-11 16:17:54 -0300

    Add Variables de Estilo, initialState.json y React Hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -8,32 +8,41 @@ import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="app">
-        <Header />
-        <Search />
-        <Categories>
-            <Carousel title="Mi Lista">
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Originales de Platzi Video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-)
+const App = () => {
+    const [videos, setVideos] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+            .then(reponse => reponse.json())
+            .then(data => setVideos(data))
+    }, [])
+    console.log(videos)
+    return (
+        <div className="app">
+            <Header />
+            <Search />
+            <Categories>
+                <Carousel title="Mi Lista">
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Categories title="Originales de Platzi Video">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+    )
+}
 
 export default App
