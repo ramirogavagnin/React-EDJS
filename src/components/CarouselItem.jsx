c751fd440934cfc0d425a629ed2bf61104c2ee29
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2019-09-11 13:40:29 -0300

    Static Assets & Footer

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import '../assets/styles/components/CarouselItem.scss'
+import playIcon from '../assets/static/play-icon.png'
+import plusIcon from '../assets/static/plus-icon.png'
 
 const CarouselItem = () => (
-    <div class="carousel-item">
+    <div className="carousel-item">
         <img
-            class="carousel-item__img"
+            className="carousel-item__img"
             src="https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260"
             alt=""
         />
-        <div class="carousel-item__details">
+        <div className="carousel-item__details">
             <div>
                 <img
-                    class="carousel-item__details--img"
-                    src="../assets/play-icon.png"
+                    className="carousel-item__details--img"
+                    src={playIcon}
                     alt="Play Icon"
                 />
                 <img
-                    class="carousel-item__details--img"
-                    src="../assets/plus-icon.png"
+                    className="carousel-item__details--img"
+                    src={plusIcon}
                     alt="Plus Icon"
                 />
             </div>
-            <p class="carousel-item__details--title">Título descriptivo</p>
-            <p class="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+            <p className="carousel-item__details--title">Título descriptivo</p>
+            <p className="carousel-item__details--subtitle">
+                2019 16+ 114 minutos
+            </p>
         </div>
     </div>
 )
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
+import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss'
 
@@ -12,13 +13,26 @@ const App = () => (
         <Header />
         <Search />
         <Categories>
-            <Carousel>
+            <Carousel title="Mi Lista">
                 <CarouselItem />
                 <CarouselItem />
                 <CarouselItem />
                 <CarouselItem />
             </Carousel>
         </Categories>
+        <Categories title="Tendencias">
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        <Categories title="Originales de Platzi Video">
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        <Footer />
     </div>
 )
 
